abb260529e5eb8330e23b19332f76089f3f36f50
Zerthys <116319911+Zerthys@users.noreply.github.com>
2022-11-28 00:18:21 +0100

    Add random selector for bot player and link script

---
+++ script.js
@@ -0,0 +1,6 @@
+const gameChoices = ["Rock","Paper","Scissors"];
+let computerChoice = null;
+
+function getComputerChoice() {
+    let computerChoice = gameChoices[Math.floor(Math.random()*myArray.length)];
+}
