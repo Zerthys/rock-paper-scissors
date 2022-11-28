ecc3523971d57ace7deaab1e9752518ed80ed32a
Zerthys <116319911+Zerthys@users.noreply.github.com>
2022-11-28 01:22:23 +0100

    Add global roundsPlayed variable

--- script.js
+++ script.js
@@ -17,30 +17,38 @@ function playRound() {
     a = getComputerChoice();
     b = getPlayerChoice();
     if (a === b) {
+        ++roundsPlayed;
         return ("It was a draw!");
     } else if (a === "ROCK" && b === "PAPER") {
         ++playerScore;
+        ++roundsPlayed;
         return ("Paper beats rock! Player wins!");
     } else if (a === "PAPER" && b === "SCISSORS") {
         ++playerScore;
+        ++roundsPlayed;
         return ("Scissors beats paper! Player wins!");
     } else if (a === "SCISSORS" && b === "ROCK") {
         ++playerScore;
+        ++roundsPlayed;
         return ("Rock beats scissors! Player wins!");
     } else if (a === "PAPER" && b === "ROCK") {
         ++computerScore;
+        ++roundsPlayed;
         return ("Paper beats rock! Computer wins!");
     } else if (a === "SCISSORS" && b === "PAPER") {
         ++computerScore;
+        ++roundsPlayed;
         return ("Scissors beats paper! Computer wins!");
     } else if (a === "ROCK" && b === "SCISSORS") {
         ++computerScore;
+        ++roundsPlayed;
         return ("Rock beats scissors! Computer wins!");
     } else {
         console.log("There was an error.");
     }
 }
 
+let roundsPlayed = 0;
 let computerScore = 0;
 let playerScore = 0;
 
